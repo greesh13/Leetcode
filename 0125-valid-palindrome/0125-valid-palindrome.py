2f949474210dc1e25dba44b7e5b233c421a5f3cd
Greeshma Yaluru <100567891+greesh13@users.noreply.github.com>
2024-05-14 12:41:36 -0400

    Time: 47 ms (45.38%), Space: 18.1 MB (21.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -10,18 +10,7 @@ class Solution:
 
 
 
-#     s= re.sub(r'[^a-zA-Z0-9]', '', s)
-#         s=s.lower()
-#         rev_s=s[::-1]
-        
-#         if s==rev_s:
-#             return True
-#         else:
-#             return False
-    
-    
-    
-    
+
             
             
             
